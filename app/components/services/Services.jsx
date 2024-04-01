3b78442a7885357a7795438badc14f9fb7c42892
Shivansh mehta <112550037+ShivanshMehtaa@users.noreply.github.com>
2024-04-01 18:03:57 +0530

    Update Services.jsx

--- app/components/services/Services.jsx
+++ app/components/services/Services.jsx
@@ -16,15 +16,15 @@ const Services = () => {
         </Link>
         <Link href="/services" className={`btn ${styles.service}`}>
           <span className={styles.icon}><FaCode/></span>
-          <h6 className={styles.title}>FrontEnd Dev.</h6>
+          <h6 className={styles.title}>FullStack Dev.</h6>
         </Link>
         <Link href="/services" className={`btn ${styles.service}`}>
           <span className={styles.icon}><FaServer/></span>
-          <h6 className={styles.title}>Backend Dev.</h6>
+          <h6 className={styles.title}>Machine Learning</h6>
         </Link>
         <Link href="/services" className={`btn ${styles.service}`}>
           <span className={styles.icon}><FaLaptopCode/></span>
-          <h6 className={styles.title}>FullStack Dev.</h6>
+          <h6 className={styles.title}>Project Management</h6>
         </Link>
       </div>
       
